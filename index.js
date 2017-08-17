e0ce4fac4f62753d625b3787f5edebd8fb2d52b7
Bergur Hallgrimsson <bergur@bergur.is>
2017-08-17 23:33:08 +0000

    Functions for flattening and making tuples

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-module.exports = exports = parameterize = function(string) {
-    const arr = string.split('?');
 
-    if (arr.length === 1) {
-        return string;
-    } else {
-        arr.pop();
-        return arr.reduce(function(newString, queryPart, index) {
-            return newString + queryPart + '$' +(index+1);
-        },'');
-    }    
+const parameterize = function(string) {
+    
+    let index = 1;
+    while(string.indexOf('?') !== -1) {
+        string = string.replace('?','$'+index);
+        index++;
+    }
+
+    return string;
 }
+
+const flatten = function(arr) {
+    return arr.reduce(function(flattened,item) {
+        item.forEach(function(value) {
+            flattened.push(value);
+        });
+        
+        return flattened;
+    },[])
+}
+
+const tuple = function(arr, parameterize) {
+    const tuple = arr.map(function(item) {        
+	    return '(' + item.map(value => '?').join(',') +')';
+    }).join(',');
+    
+    if (!parameterize) {
+        return tuple;
+    } else {                
+        return this.parameterize(tuple);        
+    }
+}
+
+exports.parameterize = parameterize;
+exports.flatten = flatten;
+exports.tuple = tuple;
--- test.js
+++ test.js
@@ -1,22 +1,68 @@
-const parameterize = require('./index');
+const queryHelper = require('./index');
 const expect = require('chai').expect;
 
-describe('pg-parameterize', function() {
+describe('parameterize', function() {
     it('should return original string when no ? parameter is found', function() {
         const string = 'SELECT * FROM table WHERE field = value';
-        const p = parameterize(string);
-        expect(p).to.equal(string);
+        const res = queryHelper.parameterize(string);
+        expect(res).to.equal(string);
     })
 
     it('should replace ? with $1', function() {
         const string = 'SELECT * FROM table WHERE field = ?';        
-        const p = parameterize(string);
-        expect(p).to.equal('SELECT * FROM table WHERE field = $1');
+        const res = queryHelper.parameterize(string);
+        expect(res).to.equal('SELECT * FROM table WHERE field = $1');
     })
 
     it('should handle multiple occurances', function() {
         const string = 'SELECT * FROM table WHERE field1 = ? OR field2 = ? AND field3 = ?';
-        const p = parameterize(string);
-        expect(p).to.equal('SELECT * FROM table WHERE field1 = $1 OR field2 = $2 AND field3 = $3');
+        const res = queryHelper.parameterize(string);
+        expect(res).to.equal('SELECT * FROM table WHERE field1 = $1 OR field2 = $2 AND field3 = $3');
     })
+})
+
+describe('flatten', function() {
+    it('should return empty array when given array is empty', function() {
+        const arr = [];
+        const res = queryHelper.flatten(arr);
+        expect(res).to.eql([]);
+    })
+
+    it('should flatten array', function() {
+        const arr = [
+    	    ['John','A',1],
+            ['Jane','B',2],
+        ];
+
+        const res = queryHelper.flatten(arr);
+        expect(res).to.eql(['John', 'A', 1, 'Jane', 'B', 2]);
+    })
+})
+
+describe('tuple', function() {    
+    it('should return empty string when given array is empty', function() {
+        const arr = [];
+        const res = queryHelper.tuple(arr);
+        expect(res).to.eql('');
+    });
+
+    it('should return tuple from array', function() {
+        const arr = [
+            ['John','A',1],
+            ['Jane','B',2]
+        ];
+
+        const res = queryHelper.tuple(arr);
+        expect(res).to.equal('(?,?,?),(?,?,?)');
+    });    
+
+    it('should return parameterized tuple from array', function() {
+        const arr = [
+            ['John','A',1],
+            ['Jane','B',2]
+        ];
+
+        const res = queryHelper.tuple(arr, true);        
+        expect(res).to.eql('($1,$2,$3),($4,$5,$6)');
+    });  
 })
