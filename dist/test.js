3381f7e3aa1f170c45cbf6496f13da695a252d72
Bergur Hallgrímsson <bergur@bergur.is>
2019-08-23 11:36:57 +0000

    Switching to jest and adding coverage

--- dist/test.js
+++ dist/test.js
@@ -1,65 +1,59 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
-const chai_1 = require("chai");
 describe('toOrdinal', () => {
-    it('should return original string when no ? parameter is found', () => {
+    test('should return original string when no ? parameter is found', () => {
         const sql = 'SELECT * FROM table WHERE field = value';
         const res = index_1.toOrdinal(sql);
-        chai_1.expect(res).to.equal(sql);
+        expect(res).toEqual(sql);
     });
-    it('should replace ? with $1', () => {
+    test('should replace ? wtesth $1', () => {
         const sql = 'SELECT * FROM table WHERE field = ?';
         const res = index_1.toOrdinal(sql);
-        chai_1.expect(res).to.equal('SELECT * FROM table WHERE field = $1');
+        expect(res).toEqual('SELECT * FROM table WHERE field = $1');
     });
-    it('should handle multiple occurances', () => {
+    test('should handle multiple occurances', () => {
         const sql = 'SELECT * FROM table WHERE field1 = ? OR field2 = ? AND field3 = ?';
         const res = index_1.toOrdinal(sql);
-        chai_1.expect(res).to.equal('SELECT * FROM table WHERE field1 = $1 OR field2 = $2 AND field3 = $3');
+        expect(res).toEqual('SELECT * FROM table WHERE field1 = $1 OR field2 = $2 AND field3 = $3');
     });
 });
 describe('flatten', () => {
-    it('should return empty array when given array is empty', () => {
+    test('should return empty array when given array is empty', () => {
         const arr = [];
         const res = index_1.flatten(arr);
-        chai_1.expect(res).to.eql([]);
+        expect(res).toEqual([]);
     });
-    it('should flatten array', () => {
+    test('should flatten array', () => {
         const arr = [
             ['John', 'A', 1],
             ['Jane', 'B', 2]
         ];
         const res = index_1.flatten(arr);
-        chai_1.expect(res).to.eql(['John', 'A', 1, 'Jane', 'B', 2]);
+        expect(res).toEqual(['John', 'A', 1, 'Jane', 'B', 2]);
     });
 });
 describe('tuple', () => {
-    it('should return empty string when given array is empty', () => {
+    test('should return empty string when given array is empty', () => {
         const arr = [];
         const res = index_1.toTuple(arr);
-        chai_1.expect(res).to.eql('');
+        expect(res).toEqual('');
     });
-    it.skip('should return tuple frome single array', () => {
-        const arr = ['John', 'A', 1];
-        const res = index_1.toTuple(arr);
-        chai_1.expect(res).to.eql('(?,?,?)');
-    });
-    it('should return tuple from array', () => {
+    test('should return tuple from array', () => {
         const arr = [
             ['John', 'A', 1],
             ['Jane', 'B', 2]
         ];
         const res = index_1.toTuple(arr);
-        chai_1.expect(res).to.equal('(?,?,?),(?,?,?)');
+        expect(res).toEqual('(?,?,?),(?,?,?)');
     });
-    it('should return parametrized tuple from array', () => {
+    test('should return parametrized tuple from array', () => {
         const arr = [
             ['John', 'A', 1],
             ['Jane', 'B', 2]
         ];
         const res = index_1.toTuple(arr, true);
-        chai_1.expect(res).to.eql('($1,$2,$3),($4,$5,$6)');
+        expect(res).toEqual('($1,$2,$3),($4,$5,$6)');
     });
 });
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,74 +1,67 @@
 import { toOrdinal, flatten, toTuple } from './index'
-import { expect } from 'chai'
 
-describe('toOrdinal', () => {        
-  it('should return original string when no ? parameter is found', () => {
+describe('toOrdinal', () => {
+  test('should return original string when no ? parameter is found', () => {
     const sql = 'SELECT * FROM table WHERE field = value'
     const res = toOrdinal(sql)
-    expect(res).to.equal(sql)
+    expect(res).toEqual(sql)
   })
 
-  it('should replace ? with $1', () => {
+  test('should replace ? wtesth $1', () => {
     const sql = 'SELECT * FROM table WHERE field = ?'
     const res = toOrdinal(sql)
-    expect(res).to.equal('SELECT * FROM table WHERE field = $1')
+    expect(res).toEqual('SELECT * FROM table WHERE field = $1')
   })
 
-  it('should handle multiple occurances', () => {
+  test('should handle multiple occurances', () => {
     const sql = 'SELECT * FROM table WHERE field1 = ? OR field2 = ? AND field3 = ?'
     const res = toOrdinal(sql)
-    expect(res).to.equal('SELECT * FROM table WHERE field1 = $1 OR field2 = $2 AND field3 = $3')
+    expect(res).toEqual('SELECT * FROM table WHERE field1 = $1 OR field2 = $2 AND field3 = $3')
   })
 })
 
 describe('flatten', () => {
-  it('should return empty array when given array is empty', () => {
+  test('should return empty array when given array is empty', () => {
     const arr = []
     const res = flatten(arr)
-    expect(res).to.eql([])
+    expect(res).toEqual([])
   })
 
-  it('should flatten array', () => {
+  test('should flatten array', () => {
     const arr = [
       ['John','A',1],
       ['Jane','B',2]
     ]
 
     const res = flatten(arr)
-    expect(res).to.eql(['John', 'A', 1, 'Jane', 'B', 2])
+    expect(res).toEqual(['John', 'A', 1, 'Jane', 'B', 2])
   })
 })
 
 describe('tuple', () => {
-  it('should return empty string when given array is empty', () => {
+  test('should return empty string when given array is empty', () => {
     const arr = []
     const res = toTuple(arr)
-    expect(res).to.eql('')
+    expect(res).toEqual('')
   })
 
-  it.skip('should return tuple frome single array', () => {
-    const arr = ['John','A',1]
-    const res = toTuple(arr)
-    expect(res).to.eql('(?,?,?)')
-  })
-
-  it('should return tuple from array', () => {
+  test('should return tuple from array', () => {
     const arr = [
       ['John','A',1],
       ['Jane','B',2]
     ]
 
     const res = toTuple(arr)
-    expect(res).to.equal('(?,?,?),(?,?,?)')
+    expect(res).toEqual('(?,?,?),(?,?,?)')
   })
 
-  it('should return parametrized tuple from array', () => {
+  test('should return parametrized tuple from array', () => {
     const arr = [
       ['John','A',1],
       ['Jane','B',2]
     ]
 
     const res = toTuple(arr, true)
-    expect(res).to.eql('($1,$2,$3),($4,$5,$6)')
+    expect(res).toEqual('($1,$2,$3),($4,$5,$6)')
   })
 })
