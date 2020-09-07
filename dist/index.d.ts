2e8e457df7ae1290a373c1c4f365485a89a02124
Bergur Hallgrímsson <bergur@bergur.is>
2020-09-07 09:58:21 +0000

    dep update, remove dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function toOrdinal(sqlString: string): string;
-export declare function flatten(arr: any[]): string;
-export declare function toTuple(arr: any[], makeOrdinal?: boolean): string;
-declare const _default: {
-    toOrdinal: typeof toOrdinal;
-    flatten: typeof flatten;
-    toTuple: typeof toTuple;
-};
-export default _default;
